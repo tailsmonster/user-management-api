477f0bad1722b14056b7b0c8edca1ccb4fb1c03c
Nico <aroca.twelve@proton.me>
2025-08-29 08:57:43 -0400

    refactored program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Register MVC controllers
 builder.Services.AddControllers();
-
-// Add OpenAPI
 builder.Services.AddOpenApi();
+builder.Services.AddLogging();
 
 var app = builder.Build();
 
-// Enable Swagger in Development
+// Middleware pipeline
+app.UseMiddleware<userMan.Middleware.ErrorHandlingMiddleware>();
+app.UseMiddleware<userMan.Middleware.TokenAuthMiddleware>();
+app.UseMiddleware<userMan.Middleware.LoggingMiddleware>();
+
+app.MapControllers();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
-// app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-// Map controller endpoints
-app.MapControllers();
 
 app.Run();
