20ef49e90ab39a6ebce101b586b0715d4a46b876
Nico <aroca.twelve@proton.me>
2025-08-29 08:43:56 -0400

    Made certain fields required

--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace userMan.Models;
 
 public class User
 {
     public int Id { get; set; }
+    
+    [Required(ErrorMessage = "First name is required")]
     public string FirstName { get; set; } = string.Empty;  
+    [Required(ErrorMessage = "Last name is required")]
     public string LastName { get; set; } = string.Empty;  
+    [Required(ErrorMessage = "Username is required")]
     public string Email { get; set; } = string.Empty;  
+    [Required(ErrorMessage = "Password is required")]
     public string Department { get; set; }= string.Empty;  
 }
